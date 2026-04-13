5aa8b5d1e03f7149c51502d40214976de7fc5181
TouchMe-exe <59952210+TouchMe-exe@users.noreply.github.com>
2026-04-14 03:04:24 +0530

    Add: boats-to-save-people - submission-5

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        
+        l = 0
+        r = len(people)-1
+        res = 0
+
+        while l <= r:
+            if people[r] + people[l] > limit:
+                r -= 1
+            else:
+                l += 1
+                r -= 1
+            res += 1
+            
+        return res 
+        
+
+        
+
+
+        
+
+        
+               
