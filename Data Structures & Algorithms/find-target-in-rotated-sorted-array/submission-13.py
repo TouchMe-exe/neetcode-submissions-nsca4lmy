1c72257c63dd6cf570d847124f0459d2fb8292b3
TouchMe-exe <59952210+TouchMe-exe@users.noreply.github.com>
2026-04-14 21:49:38 +0530

    Add: find-target-in-rotated-sorted-array - submission-13

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-13.py
@@ -0,0 +1,34 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        
+        def bsearch(l: int,r: int,target: int)->int:
+            while l<=r:
+                m = l + ((r-l)//2)
+                if nums[m] == target:
+                    return m
+                elif nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m - 1
+            return -1
+        
+        def find_pivot(l: int,r: int)->int:
+            while l<=r:
+                m = l + (r-l) // 2
+
+                if nums[m]<nums[r]:
+                    r = m
+                elif nums[m] > nums[r]:
+                    l = m + 1
+                else:
+                    return m
+            
+
+        l, r = 0, len(nums)-1
+        pivot = find_pivot(l,r)
+        
+        res = bsearch(l, pivot-1, target)
+        if res == -1:
+            res = bsearch(pivot,r,target)
+        
+        return res
